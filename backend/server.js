83cf6ca217f14ca11864009381801660efb2974b
Jonathan Yudhistira <jonathanyud@gmail.com>
2025-02-11 14:56:00 -0500

    Set up Express server with CORS and JSON middleware

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+require
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send("Backend is running!");
+});
+
+// Server port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, (error) => {
+    if(!error)
+        console.log('Server is running on port', PORT);
+    else
+        console.log("Error, server can't start:", error);
+});
