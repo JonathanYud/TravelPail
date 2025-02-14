71b82cd93316b60993c1f5b31d5a88e9c06a22c2
Jonathan Yudhistira <jonathanyud@gmail.com>
2025-02-14 16:03:36 -0500

    Remove Home page component

--- web/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-      <div className="flex justify-center items-center h-screen bg-gray-100">
-        <h1 className="text-3xl font-bold">Welcome to TravelPail</h1>
-      </div>
-    );
-  }
-  
